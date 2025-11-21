e9c658b61b53c0f2eb55eea996b667b29b85fde9
pambaliharsha06 <pambaliharsha06@gmail.com>
2025-11-21 23:35:12 +0530

    print statement to data types

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        System.out.println("hi");
+        byte b=8;
+        short a= 5;
+        int d=9;
+        System.out.println("the datatypes and its default values");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        char c='H';
+        System.out.println(c);
     }
 }
